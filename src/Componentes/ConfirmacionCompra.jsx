b1bd1b43e9185abc9d45b1a8f83ab7976220af8d
EmiliaCabrera1 <emi.22692@gmail.com>
2025-03-01 21:55:19 -0300

    borro google analytics

--- src/Componentes/ConfirmacionCompra.jsx
+++ src/Componentes/ConfirmacionCompra.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ConfirmacionCompra = () => {
   return (
-    <div className="flex flex-col items-center px-30 py-20 bg-[#bbb4a9]/80 rounded-[10px] shadow-[0px_6px_6px_0px_rgba(0,0,0,0.30)] mx-auto ">
+    <div className="flex flex-col items-center px-30 py-20 bg-[#bbb4a9]/80 rounded-[10px] shadow-[0px_6px_6px_0px_rgba(0,0,0,0.30)] my-3 mx-auto ">
       <img
         src="./assets/confirmar.svg"
         alt="Compra Confirmada"
@@ -11,7 +11,7 @@ const ConfirmacionCompra = () => {
       <h6 className="mb-6 text-2xl text-center">
         GRACIAS POR SU COMPRA EN DE LAS ALBA!!
       </h6>
-      <h5 className="text-center text-xl">
+      <h5 className="text-center">
         UN VENDEDOR SE COMUNICARA A TU MAIL REGISTRADO PARA INDICARTE LAS
         INSTRUCCIONES DE PAGO Y SOLICITARTE LA DIRECCION DE ENTREGA.
       </h5>
